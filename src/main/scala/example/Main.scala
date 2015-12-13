b5f651a154311dc3064302996a54586ab7a3917a
lamrongol <lamrongol@gmail.com>
2015-12-13 18:52:13 +0900

    Add example

---
+++ src/main/scala/example/Main.scala
@@ -0,0 +1,26 @@
+package example
+
+import lamrongol.regression.{NonlinearRegressionFunctionSelectionByGeneticAlgorithm, RegressionCalculator}
+
+/**
+  * Created by admin on 2015/12/05.
+  */
+object Main extends App {
+  val outputFile = "result.tsv"
+  new NonlinearRegressionFunctionSelectionByGeneticAlgorithm("sampleData.tsv",
+    //if you know each parameter is always plus (or not), this program explores functions which allows only plus value (such as sqrt, log), too
+    isPlus = Array(true, true, true, false, true, true),
+    checkMode = true,
+    recordFile = outputFile).execute()
+
+  //You can omit arguments as possible as following
+  //new RegressionParameterSelectionByGeneticAlgorithm("sampleData.tsv").execute()
+
+
+  //calculate for new data
+  val calculator = new RegressionCalculator(outputFile)
+  println(calculator.calculate(List(500, 600, 0.12, -18, 30000, 0.00075)))
+  //You must set any value if unused parameters exist
+  //println(calculator.calculate(List(500, Double.NaN, 0.12, -18, 30000, 0.00075)))
+
+}
---
+++ src/main/scala/example/SampleDataGenerator.scala
@@ -0,0 +1,28 @@
+package example
+
+import java.io.PrintWriter
+
+import scala.util.Random
+
+/**
+  * Created by admin on 2015/12/05.
+  */
+object SampleDataGenerator extends App {
+  val DATA_NUM = 500
+
+  val pw = new PrintWriter("sampleData.tsv")
+
+  for (i <- 0 until DATA_NUM) {
+    val x1 = Random.nextDouble() * 10000
+    val x2 = 50 + Random.nextDouble() * 6000
+    val x3 = 0.10 + Random.nextDouble() / 10
+    val x4 = (Random.nextDouble() - 0.5) * 124
+    val x5 = Random.nextDouble() * 800000 + 2000
+    val x6 = (Random.nextDouble() * 9 + 1) / 8000
+    val noise = Random.nextGaussian() * 100
+
+    val y = x1 + Math.exp(72 * x3) + x4 * x4 + Math.sqrt(x5) + 1 / x6 + noise
+    pw.println(List(y, x1, x2, x3, x4, x5, x6).mkString("\t"))
+  }
+  pw.close()
+}
